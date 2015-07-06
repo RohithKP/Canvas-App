f583d343ce3cbcecd8ceb7a6329f3b35fe2fe610
Rohith KP <rohithkp.me@gmail.com>
2015-07-06 19:49:25 +0530

    function setbound was changed

--- custom.js
+++ custom.js
@@ -49,61 +49,25 @@ function onKeyDownHandler(e) {
 canvas.on("object:moving",setbound);
 
  function setbound(e) {
-  var obj = e.target;
-  var canvas = obj.canvas;
-  var top = obj.top;
-  var left = obj.left;
-  var zoom = canvas.getZoom();
-  var pan_x = canvas.viewportTransform[4];
-  var pan_y = canvas.viewportTransform[5];
-
-  // width & height we are constraining to must be calculated by applying the inverse of the current viewportTransform
-  var c_width = canvas.width / zoom;
-  var c_height = canvas.height / zoom;
-
-
-  var w = obj.width * obj.scaleX
-  var left_adjust, right_adjust
-  if(obj.originX == "center") {
-    left_adjust = right_adjust = w / 2;
-  } else {
-    left_adjust = 0;
-    right_adjust = w;
-  }
-
-  var h = obj.height * obj.scaleY;
-  var top_adjust, bottom_adjust;
-  if(obj.originY == "center") {
-    top_adjust = bottom_adjust = h / 2;
-  } else {
-    top_adjust = 0;
-    bottom_adjust = h;
-  }
-
-  // if you need margins set them here
-  var top_margin = 0;
-  var bottom_margin = 0;
-  var left_margin = 0;
-  var right_margin = 0;
-
-
-  var top_bound = top_margin + top_adjust - pan_y;
-  var bottom_bound = c_height - bottom_adjust - bottom_margin - pan_y;
-  var left_bound = left_margin + left_adjust - pan_x;
-  var right_bound = c_width - right_adjust - right_margin - pan_x;
-
-  if( w > c_width ) {
-    obj.setLeft(left_bound);
-  } else {
-    obj.setLeft(Math.min(Math.max(left, left_bound), right_bound));
-  }
-
-  if( h > c_height ) {
-    obj.setTop(top_bound);
-  } else {
-    obj.setTop(Math.min(Math.max(top, top_bound), bottom_bound));
-  }
+          var obj = e.target;
+        // if object is too big ignore
+        if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width) {
+            return;
+        }
+        obj.setCoords();
+        // top-left  corner
+        if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0) {
+            obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
+            obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
+        }
+        // bot-right corner
+        if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width) {
+            obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
+            obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
+        }
 }
+
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
